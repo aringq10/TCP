41824ef1e23735c9a495b683f0b55a047fbe1795
Aringas Civilka <aringas.civilka@eneba.com>
2026-05-11 21:02:04 +0300

    Write echo server using gorilla websocket lib

---
+++ server/go.mod
@@ -0,0 +1,5 @@
+module github.com/aringq10/TCP/server
+
+go 1.24.4
+
+require github.com/gorilla/websocket v1.5.3 // indirect
--- server/main.go
+++ server/main.go
@@ -1,9 +1,98 @@
-package server
+package main
 
 import (
-    "fmt"
+	// "fmt"
+	// "io"
+	"log"
+	// "net"
+	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
-func main()  {
-    fmt.Println("Hello World")
+const port = ":6767"
+
+var upgrader = websocket.Upgrader{
+    ReadBufferSize:  1024,
+    WriteBufferSize: 1024,
+    CheckOrigin: func(r *http.Request) bool {
+        return true // allow all origins in dev; tighten in prod
+    },
+}
+
+// func connToChan(c net.Conn, ch chan []byte) {
+//     b := make([]byte, 1024)
+//     for {
+//         n, err := c.Read(b)
+//         if err == io.EOF {
+//             break
+//         }
+//         if err != nil {
+//             fmt.Println("reading error:", err)
+//             continue
+//         }
+//
+//         out := make([]byte, n)
+//         copy(out, b)
+//
+//         ch <- out
+//     }
+// }
+//
+// func handleMatch(connWhite net.Conn, connBlack net.Conn) {
+//     defer connWhite.Close()
+//     defer connBlack.Close()
+//
+//     fmt.Println("Connecting", connWhite.RemoteAddr(), "with", connBlack.RemoteAddr())
+//
+//     chWhite := make(chan []byte)
+//     chBlack := make(chan []byte)
+//
+//     go connToChan(connWhite, chWhite)
+//     go connToChan(connBlack, chBlack)
+//
+//     fmt.Println("Starting echo loop for match")
+//     var data []byte
+//     for {
+//         select {
+//         case data = <-chWhite:
+//             fmt.Println("Received data from chWhite:", data)
+//             connBlack.Write(data)
+//         case data = <-chBlack:
+//             fmt.Println("Received data from chBlack:", data)
+//             connWhite.Write(data)
+//         }
+//     }
+// }
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+    log.Println("ws connection from", r.RemoteAddr)
+    conn, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        log.Println("upgrade failed:", err)
+        return
+    }
+    defer conn.Close()
+
+    for {
+        messageType, p, err := conn.ReadMessage()
+        if err != nil {
+            if websocket.IsCloseError(err, websocket.CloseNormalClosure, websocket.CloseGoingAway) {
+                break
+            }
+            log.Println("read error:", err)
+            break
+        }
+
+        if err := conn.WriteMessage(messageType, p); err != nil {
+            log.Println("write error:", err)
+            break
+        }
+    }
+}
+
+func main() {
+    http.HandleFunc("/ws", wsHandler)
+    log.Println("server listening on", port)
+    log.Fatal(http.ListenAndServe(port, nil))
 }
