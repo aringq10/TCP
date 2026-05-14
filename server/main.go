f4b4940dedb85f0776b95f0bf3635012ff8450ea
Aringas Civilka <aringas.civilka@eneba.com>
2026-05-14 22:51:57 +0300

    Reading up on websockets and playing with the lib

--- server/main.go
+++ server/main.go
@@ -2,15 +2,42 @@ package main
 
 import (
 	// "io"
+	"errors"
 	"fmt"
 	"log"
+	"slices"
+	"sync"
+
 	// "net"
 	"net/http"
 
 	"github.com/gorilla/websocket"
 )
 
-const PORT = 6767
+const PORT  = 6767
+const MOUNT = "/ws"
+const WS_READ_LIMIT = 64
+var conns connPool
+
+type connPool struct {
+    conns []*websocket.Conn
+    mu    sync.Mutex
+}
+
+func (p *connPool) addConn(c *websocket.Conn) {
+    p.conns = append(p.conns, c)
+}
+
+func (p *connPool) removeConn(c *websocket.Conn) {
+    if i := slices.Index(p.conns, c); i >= 0 {
+        p.conns[i] = p.conns[len(p.conns) - 1]
+        p.conns = p.conns[:len(p.conns) - 1]
+    }
+}
+
+func (p *connPool) count() int {
+    return len(p.conns)
+}
 
 var upgrader = websocket.Upgrader{
     ReadBufferSize:  1024,
@@ -65,26 +92,55 @@ var upgrader = websocket.Upgrader{
 //     }
 // }
 
+func setupConn(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
+    c, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        return nil, errors.New("upgrade failed:" + err.Error())
+    }
+
+    if conns.count() > 0 {
+        closeConn(c, "queue is full")
+        return nil, errors.New("connection ended: queues is full")
+    }
+
+    c.SetReadLimit(WS_READ_LIMIT)
+
+    conns.addConn(c)
+
+    return c, nil
+}
+
+func closeConn(c * websocket.Conn, reason string) {
+    c.WriteMessage(
+        websocket.CloseMessage,
+        websocket.FormatCloseMessage(websocket.CloseNormalClosure, reason),
+    )
+    c.Close()
+    conns.removeConn(c)
+}
+
 func wsHandler(w http.ResponseWriter, r *http.Request) {
     log.Println("ws connection from", r.RemoteAddr)
-    conn, err := upgrader.Upgrade(w, r, nil)
+
+    c, err := setupConn(w, r)
     if err != nil {
-        log.Println("upgrade failed:", err)
-        return
+        log.Println(err)
     }
-    defer conn.Close()
+
+    defer closeConn(c, "twas good playing")
 
     for {
-        messageType, p, err := conn.ReadMessage()
+        messageType, p, err := c.ReadMessage()
         if err != nil {
             if websocket.IsCloseError(err, websocket.CloseNormalClosure, websocket.CloseGoingAway) {
                 break
             }
+
             log.Println("read error:", err)
             break
         }
 
-        if err := conn.WriteMessage(messageType, p); err != nil {
+        if err := c.WriteMessage(messageType, p); err != nil {
             log.Println("write error:", err)
             break
         }
@@ -96,6 +152,6 @@ func main() {
 
     log.Println("server listening on", PORT)
 
-    http.HandleFunc("/ws", wsHandler)
-	log.Fatal(http.ListenAndServe(address, nil))
+    http.HandleFunc(MOUNT, wsHandler)
+    log.Fatal(http.ListenAndServe(address, nil))
 }
